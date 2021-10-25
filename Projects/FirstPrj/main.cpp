2a2836321b575f6b8abba2452f76edda7ed911b4
Anatoly Novikov <zeyatolyas@gmail.com>
2021-10-25 14:35:01 +0300

    smiley face on box

--- Projects/FirstPrj/main.cpp
+++ Projects/FirstPrj/main.cpp
@@ -22,7 +22,7 @@ void DrawTriangle(float* verts, unsigned int Size, unsigned int VBO)
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
-void DrawRectangle(float* verts, unsigned int* indices, unsigned int Size_verts, unsigned int Size_indices, unsigned int VBO, unsigned int EBO, unsigned int texture)
+void DrawRectangle(float* verts, unsigned int* indices, unsigned int Size_verts, unsigned int Size_indices, unsigned int VBO, unsigned int EBO)
 {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, Size_verts, verts, GL_STATIC_DRAW);
@@ -40,19 +40,17 @@ void DrawRectangle(float* verts, unsigned int* indices, unsigned int Size_verts,
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 
-void ChangeColor(float Value, unsigned int shaderProgram)
-{
-    float timeValue = glfwGetTime();
-    float greenValue = (sin(timeValue) / 2.0f) + Value;
-    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-    //glUseProgram(shaderProgram);
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-}
+//void ChangeColor(float Value, unsigned int shaderProgram)
+//{
+//    float timeValue = glfwGetTime();
+//    float greenValue = (sin(timeValue) / 2.0f) + Value;
+//    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+//    //glUseProgram(shaderProgram);
+//    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+//}
 
 
 int main()
@@ -81,12 +79,15 @@ int main()
 
     Shader CustomShaders("Shaders/shader.vs", "Shaders/shader.fs");
 
+    stbi_set_flip_vertically_on_load(true);
     int width, height, nrChannels;
     unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
         
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -96,6 +97,23 @@ int main()
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
@@ -122,18 +140,19 @@ int main()
 
     glBindVertexArray(VAO);
 
-
     while (!glfwWindowShouldClose(window))
     {
         CustomShaders.use();
-        float offset = 0.3f;
-        CustomShaders.setFloat("xOffset", offset);
+        CustomShaders.setInt("texture1", 0);      
+        CustomShaders.setInt("texture2", 1);       
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        DrawRectangle(vertices, indices, sizeof(vertices), sizeof(indices), VBO, EBO, texture);
+        DrawRectangle(vertices, indices, sizeof(vertices), sizeof(indices), VBO, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
