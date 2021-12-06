0f486b75e208b022e361fba2d2140cee7952ab40
Anatoly <zeyatolyas@gmail.com>
2021-12-06 20:32:16 +0300

    camera movement

--- Projects/FirstPrj/main.cpp
+++ Projects/FirstPrj/main.cpp
@@ -6,6 +6,13 @@
 
 void processInput(GLFWwindow* window, float& transparency);
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
 int main()
 {
     glfwInit();
@@ -92,8 +99,6 @@ int main()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     glm::mat4 projection;
 
     glm::vec3 cubePositions[] = 
@@ -114,18 +119,22 @@ int main()
     CustomShaders.setTexture("container.jpg", 0);
     CustomShaders.setTexture("awesomeface.png", 1, true);
 
-    //CustomShaders.setInt("texture1", 0);      
-    //CustomShaders.setInt("texture2", 1);  
-    float FovChanging = 60.0f;
-    bool isFovIncrementing = true;
+
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         processInput(window, transparency);
         CustomShaders.setFloat("transparency", transparency);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         CustomShaders.setMat4("view", view);
 
         glBindVertexArray(VAO);
@@ -133,31 +142,22 @@ int main()
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i - 1]);
-            float angle = 20.0f * i;
+            float angle;
+            if (i % 3 != 0)
+            {
+                angle = 20.0f * i;
+            }
+            else
+            {
+                angle = 0;
+            }
             model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             CustomShaders.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        if (FovChanging < 80.0f && isFovIncrementing)
-        {
-            FovChanging += 0.01f;
-        }
-        else if (isFovIncrementing)
-        {
-            isFovIncrementing = false;
-        }
-        if (FovChanging > 50.0f && !isFovIncrementing)
-        {
-            FovChanging -= 0.01f;
-        }
-        else if (!isFovIncrementing)
-        {
-            isFovIncrementing = true;
-        }
-
-        projection = glm::perspective(glm::radians(FovChanging), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(60.0f), 800.0f / 600.0f, 0.1f, 100.0f);
         CustomShaders.setMat4("projection", projection);
 
         glfwSwapBuffers(window);
@@ -175,7 +175,16 @@ void processInput(GLFWwindow* window, float& transparency)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        transparency += 0.1;
+        transparency += 0.01f;
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        transparency -= 0.1;
+        transparency -= 0.01f;
+    const float cameraSpeed = 3.0f * deltaTime; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
